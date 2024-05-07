6c5bcf2b27dd6e21118782e4c65c4116d6013f94
Antonio Escamilla <7999008+AntonioEscamilla@users.noreply.github.com>
2024-05-07 08:58:54 -0500

    Hover over the spectrogram

--- src/plugins/hover.ts
+++ src/plugins/hover.ts
@@ -1,5 +1,9 @@
 /**
  * The Hover plugin follows the mouse and shows a timestamp
+ * Note for me: 
+ * access the  WaveSurfer.js directory
+ * install dependencies running: npm install --force
+ * build the WaveSurfer.js library by running the build script: npm run build
  */
 
 import BasePlugin, { type BasePluginEvents } from '../base-plugin.js'
@@ -11,19 +15,11 @@ export type HoverPluginOptions = {
   labelColor?: string
   labelSize?: string | number
   labelBackground?: string
-  /* Allows to customize time label */
-  formatTimeCallback?: (seconds: number) => string
 }
 
 const defaultOptions = {
   lineWidth: 1,
   labelSize: 11,
-  formatTimeCallback(seconds: number) {
-    const minutes = Math.floor(seconds / 60)
-    const secondsRemainder = Math.floor(seconds) % 60
-    const paddedSeconds = `0${secondsRemainder}`.slice(-2)
-    return `${minutes}:${paddedSeconds}`
-  },
 }
 
 export type HoverPluginEvents = BasePluginEvents & {
@@ -35,6 +31,11 @@ class HoverPlugin extends BasePlugin<HoverPluginEvents, HoverPluginOptions> {
   private wrapper: HTMLElement
   private label: HTMLElement
   private unsubscribe: () => void = () => undefined
+  // Declare horizontalLine property
+  private horizontalLine: HTMLElement 
+  private freq_label: HTMLElement
+  private spectrogramContainer: HTMLElement | null
+
 
   constructor(options?: HoverPluginOptions) {
     super(options || {})
@@ -43,6 +44,12 @@ class HoverPlugin extends BasePlugin<HoverPluginEvents, HoverPluginOptions> {
     // Create the plugin elements
     this.wrapper = createElement('div', { part: 'hover' })
     this.label = createElement('span', { part: 'hover-label' }, this.wrapper)
+
+    // Create the plugin elements
+    this.horizontalLine = createElement('div', { part: 'hover' })
+    this.freq_label = createElement('span', { part: 'hover-label' }, this.wrapper)
+
+    this.spectrogramContainer = document.querySelector('#spectrogram-container')
   }
 
   public static create(options?: HoverPluginOptions) {
@@ -75,7 +82,6 @@ class HoverPlugin extends BasePlugin<HoverPluginEvents, HoverPluginOptions> {
       opacity: '0',
       transition: 'opacity .1s ease-in',
     })
-
     // Timestamp label
     Object.assign(this.label.style, {
       display: 'block',
@@ -86,9 +92,36 @@ class HoverPlugin extends BasePlugin<HoverPluginEvents, HoverPluginOptions> {
       padding: '2px 3px',
     })
 
+    // Horizontal line
+    Object.assign(this.horizontalLine.style, {
+      position: 'absolute',
+      zIndex: 10,
+      left: 0,
+      top: 0,
+      width: '100%',
+      pointerEvents: 'none',
+      borderBottom: `${this.addUnits(this.options.lineWidth)} solid ${lineColor}`,
+      opacity: '0',
+      transition: 'opacity .1s ease-in',
+    })
+
+    // Freqstamp label
+    Object.assign(this.freq_label.style, {
+      display: 'block',
+      backgroundColor: this.options.labelBackground,
+      color: this.options.labelColor,
+      fontSize: `${this.addUnits(this.options.labelSize)}`,
+      transition: 'opacity .1s ease-in',
+      padding: '2px 3px',
+    })
+
     // Append the wrapper
     const container = this.wavesurfer.getWrapper()
     container.appendChild(this.wrapper)
+
+    // Append the H-line
+    container.appendChild(this.horizontalLine)
+
 
     // Attach pointer events
     container.addEventListener('pointermove', this.onPointerMove)
@@ -99,6 +132,13 @@ class HoverPlugin extends BasePlugin<HoverPluginEvents, HoverPluginOptions> {
       container.removeEventListener('pointerleave', this.onPointerLeave)
       container.removeEventListener('wheel', this.onPointerLeave)
     }
+  }
+
+  private formatTime(seconds: number): string {
+    const minutes = Math.floor(seconds / 60)
+    const secondsRemainder = Math.floor(seconds) % 60
+    const paddedSeconds = `0${secondsRemainder}`.slice(-2)
+    return `${minutes}:${paddedSeconds}`
   }
 
   private onPointerMove = (e: PointerEvent | WheelEvent) => {
@@ -112,13 +152,34 @@ class HoverPlugin extends BasePlugin<HoverPluginEvents, HoverPluginOptions> {
     const posX = Math.min(width - this.options.lineWidth - 1, offsetX)
     this.wrapper.style.transform = `translateX(${posX}px)`
     this.wrapper.style.opacity = '1'
-
     // Timestamp
     const duration = this.wavesurfer.getDuration() || 0
-    this.label.textContent = this.options.formatTimeCallback(duration * relX)
+    this.label.textContent = this.formatTime(duration * relX)
     const labelWidth = this.label.offsetWidth
-    this.label.style.transform =
-      posX + labelWidth > width ? `translateX(-${labelWidth + this.options.lineWidth}px)` : ''
+    this.label.style.transform = posX + labelWidth > width ? `translateX(-${labelWidth + this.options.lineWidth}px)` : ''
+
+    // Position for horizontal line
+    const { height } = bbox
+    const offsetY = e.clientY - bbox.top;                                 // Calculate mouse position relative to the top of the waveform
+    const posY = Math.min(height - this.options.lineWidth - 1, offsetY);  // Adjust positioning for the horizontal line
+    
+    // Freq label
+    const relY = Math.min(1, Math.max(0, (offsetY  - 200) / 300))         // Relative Y position inside the spectrogram between (0, 1)
+    if (relY === 0) {
+      this.freq_label.textContent = '';                                   // Hide the label by setting an empty string as its content
+      this.freq_label.style.display = 'none';                             // Optionally, hide the label by setting its display property to 'none'
+      this.horizontalLine.style.opacity = '0';                            // Hide the horizontal line by setting its opacity to 0
+    } else {
+      const frequency = ((1 - relY) * 22050).toFixed(0);
+      this.freq_label.textContent = `${frequency} Hz`;
+      this.freq_label.style.display = 'block';
+      this.horizontalLine.style.transform = `translateY(${posY}px)`;      // Update the position of the horizontal line
+      this.horizontalLine.style.opacity = '1'                             // Show the horizontal line by setting its opacity to 1
+    }
+    const freq_labelWidth = this.freq_label.offsetWidth                   // Calculate label width and check if it would overflow
+    const freq_labelHeight = this.freq_label.offsetHeight
+    const overflowTranslateX = posX + freq_labelWidth > width ? -freq_labelWidth - this.options.lineWidth : 0;
+    this.freq_label.style.transform =  `translate(${overflowTranslateX}px, ${posY - 2*freq_labelHeight}px)`
 
     // Emit a hover event with the relative X position
     this.emit('hover', relX)
@@ -126,6 +187,7 @@ class HoverPlugin extends BasePlugin<HoverPluginEvents, HoverPluginOptions> {
 
   private onPointerLeave = () => {
     this.wrapper.style.opacity = '0'
+    this.horizontalLine.style.opacity = '0'
   }
 
   /** Unmount */
