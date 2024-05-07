e23c4345a714304c69bee285572b5a24868d30e2
Antonio Escamilla <7999008+AntonioEscamilla@users.noreply.github.com>
2024-05-07 09:01:16 -0500

    dist files added
    dist folder removed from the .gitignore

---
+++ dist/plugins/plugins/hover.d.ts
@@ -0,0 +1,41 @@
+/**
+ * The Hover plugin follows the mouse and shows a timestamp
+ * Note for me:
+ * access the  WaveSurfer.js directory inside the Score_Following project
+ * build the WaveSurfer.js library by running the build script: npm run build
+ */
+import BasePlugin, { type BasePluginEvents } from '../base-plugin.js';
+export type HoverPluginOptions = {
+    lineColor?: string;
+    lineWidth?: string | number;
+    labelColor?: string;
+    labelSize?: string | number;
+    labelBackground?: string;
+};
+declare const defaultOptions: {
+    lineWidth: number;
+    labelSize: number;
+};
+export type HoverPluginEvents = BasePluginEvents & {
+    hover: [relX: number];
+};
+declare class HoverPlugin extends BasePlugin<HoverPluginEvents, HoverPluginOptions> {
+    protected options: HoverPluginOptions & typeof defaultOptions;
+    private wrapper;
+    private label;
+    private unsubscribe;
+    private horizontalLine;
+    private freq_label;
+    private spectrogramContainer;
+    constructor(options?: HoverPluginOptions);
+    static create(options?: HoverPluginOptions): HoverPlugin;
+    private addUnits;
+    /** Called by wavesurfer, don't call manually */
+    onInit(): void;
+    private formatTime;
+    private onPointerMove;
+    private onPointerLeave;
+    /** Unmount */
+    destroy(): void;
+}
+export default HoverPlugin;
